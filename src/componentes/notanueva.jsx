d0836e0c7d0b1501e2ffdef92112c9bde3c48659
Dari023 <dariocahuachia@gmail.com>
2024-07-14 19:30:19 -0400

    commit

--- src/componentes/notanueva.jsx
+++ src/componentes/notanueva.jsx
@@ -1,11 +1,20 @@
+export const NotaNueva = ({ nota }) => {
+    const color = nota.importante;
+    if (color) {
+        return (
+            <div className="nota-importante">
+                <h2>{nota.titulo}</h2>
+                <p>{nota.descripcion}</p>
+            </div>
+        );
+    }
+    return (
+            <div className="nota-Noimportante">
+                <h2>{nota.titulo}</h2>
+                <p>{nota.descripcion}</p>
+            </div>
+        );
+    
+}
 
-import React from 'react';
-
-
-export const NotaNueva = ({ nota }) => (
-    <div>
-        <h2>{nota.titulo}</h2>
-        <p>{nota.descripcion}</p>
-    </div>
-);
 
--- src/componentes/notas.jsx
+++ src/componentes/notas.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { NotaNueva } from "./notanueva";
+import "../css/app.css"
 
 export const Notas = () => {
     const tituloRef = useRef();
@@ -13,17 +14,31 @@ export const Notas = () => {
             descripcion: descricionRef.current.value,
             importante: esImportante
         };
+        console.log(nuevaNota);
         setNotas([...notas, nuevaNota]);
+       
         tituloRef.current.value = '';
         descricionRef.current.value = '';
         setEsImportante(false);
     }
+    
+    // para saber si es true o false
+    const importanteSioNO = (event) => {
+        const marca =  event.target.checked;
 
-  
-    alert("agrego una nota nueva")
+        if (marca === true) {
+            setEsImportante(true);
+        } else {
+            setEsImportante(false);
+        }
+    }
+    
+    
+    
+    // alert("agrego una nota nueva")
 
     return(
-        <div>
+        <div className="principal">
             <h1>Post It Simulator</h1>
             <div>
                 <input ref={tituloRef} type="text" placeholder="titulo"/>
@@ -32,11 +47,11 @@ export const Notas = () => {
                 <input ref={descricionRef} type="text" placeholder="descricion"/>
             </div>
             <label>
-                <input type="checkbox" checked={esImportante}/>
+                <input type="checkbox" checked={esImportante} onChange={importanteSioNO}/>
             </label> importante
             <button type="submit" onClick={agregarNota}>Agregar Nota</button>
             <div>
-                {notas.map(nota => <NotaNueva key={nota.titulo} nota={nota} />)}
+                {notas.map(nota => <NotaNueva key={nota.titulo} nota={nota}/>)}
             </div>
 
         </div>
